45e9adda849901c516bb040e15b80b13ba3316ad
bhanna1693 <bhanna1693@gmail.com>
2019-03-03 17:21:42 -0500

    todo-list MVC functionality

---
+++ src/main/java/academy/learnprogramming/service/TodoService.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.service;
+
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
+
+public interface TodoService {
+
+    // == fields ==
+    void addItem(TodoItem toAdd);
+    void removeItem(int id);
+    TodoItem getItem(int id);
+    void updateItem(TodoItem toUpdate);
+    TodoData getData();
+}
